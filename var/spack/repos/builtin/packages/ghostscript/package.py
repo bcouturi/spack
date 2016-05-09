614fcb285b0dc50d6eafca3778459664a61a1e86
Ben Couturier <ben.couturier@cern.ch>
2016-05-09 22:16:56 +0200

    Added gdb 7.11

--- var/spack/repos/builtin/packages/ghostscript/package.py
+++ var/spack/repos/builtin/packages/ghostscript/package.py
@@ -3,9 +3,10 @@ from spack import *
 class Ghostscript(Package):
     """an interpreter for the PostScript language and for PDF. """
     homepage = "http://ghostscript.com/"
-    url      = "http://downloads.ghostscript.com/public/ghostscript-9.16.tar.gz"
-
-    version('9.16', '829319325bbdb83f5c81379a8f86f38f')
+    #url      = "http://downloads.ghostscript.com/public/ghostscript-9.16.tar.gz"
+    url      = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs919/ghostscript-9.19.tar.gz"
+    
+    version('9.19', '829319325bbdb83f5c81379a8f86f38f')
 
     parallel = False
 
